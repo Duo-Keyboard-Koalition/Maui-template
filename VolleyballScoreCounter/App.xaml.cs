e97193ed3eaa6733f12b6936eae9710603f35ae3
Darcy Liu <darcy.ldx@gmail.com>
2024-08-10 22:03:16 -0400

    Add project files.

---
+++ VolleyballScoreCounter/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using VolleyballScoreCounter;
+using Microsoft.Maui.Controls;
+namespace VolleyballScoreCounter
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ VolleyballScoreCounter/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using VolleyballScoreCounter;
+using Microsoft.Maui.Controls;
+
+namespace VolleyballScoreCounter
+{
+    public partial class MainPage : ContentPage
+    {
+        int redTeamScore = 0;
+        int blueTeamScore = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void onRedClicked(object sender, EventArgs e)
+        {
+            redTeamScore++;
+            RedBtn.Text = $"Red Team Scored: {redTeamScore}";
+
+            SemanticScreenReader.Announce("Red Team scored!");
+        }
+
+        private void onBlueClicked(object sender, EventArgs e)
+        {
+            blueTeamScore++;
+            BlueBtn.Text = $"Blue Team Scored: {blueTeamScore}";
+
+            SemanticScreenReader.Announce("Blue Team scored!");
+        }
+    }
+}
